2e3d727aa0864112a04c3cdaa7cf602857d0e663
Tuan Khoa <nhimspec@gmail.com>
2016-11-23 16:26:43 +0700

    Đó

--- layout/js/custom.js
+++ layout/js/custom.js
@@ -29,14 +29,20 @@ $( function() {
         var image = $product.find("image").text();
         var name = $product.find("name").text();
         var price = $product.find("price").text();
+        var sales = $product.find("sales").text();
         var cat = $product.find("category").text() + ' ';
         var size = $product.find("size").text()+ ' ';
         var brand = $product.find("brand").text();
+        var description = $product.find("description").text();
 
-        var html = '<div class="col-md-4 product-elements ' +cat+size+brand+ '"><a href="show-product.html"><div class="product-single">';
-        html += '<img src="layout/images/' + image + '">';
+        var html = '<div class="col-md-4 product-elements ' +cat+size+brand+ '"><a class="tap"><div class="product-single">';
+        html += '<img src="layout/images/products/' + image + '">';
         html += '<h3>' + name + '</h3>';
         html += '<span>' + price + '$</span>';
+        if ( sales != '' ){
+          html += '<span class="sales" style="margin-left:30px;">' + sales + '</span>';
+        }
+        html += '<input type="hidden" value="'+ description +'">';
         html += '</div></a></div>';
 
         $('#content .single > .row').append($(html));   
@@ -48,16 +54,48 @@ $( function() {
           var image = $product.find("image").text();
           var name = $product.find("name").text();
           var price = $product.find("price").text();
+          var sales = $product.find("sales").text();
+          var description = $product.find("description").text();
 
-          var html = '<div class="col-md-12"><a href="show-product.html"><div class="product">';
-          html += '<img src="layout/images/' + image + '">';
+          var html = '<div class="col-md-12"><a class="tap"><div class="product">';
+          html += '<img src="layout/images/products/' + image + '">';
           html += '<h3>' + name + '</h3>';
           html += '<span>' + price + '$</span>';
+          if ( sales != '' ){
+            html += '<span class="sales" style="margin-left:30px;">' + sales + '</span>';
+          }
+          html += '<input type="hidden" value="'+ description +'">';
           html += '</div></a></div>';
           
           $('#content .products').append($(html));   
         });
 
+      // Best Product
+      var i=0;
+      $(d).find('product').each(function(){
+          i++;
+          if ( i <= 3) {
+            var $product = $(this); 
+            var image = $product.find("image").text();
+            var name = $product.find("name").text();
+            var price = $product.find("price").text();
+            var sales = $product.find("sales").text();
+            var description = $product.find("description").text();
+
+            var html = '<div class="col-sm-4 item"><a class="tap"><div class="product-sale clearfix">';
+            html += '<img src="layout/images/products/' + image + '">';
+            html += '<div class="extra-product-sales">';
+            html += '<h3>' + name + '</h3>';
+            html += '<span>' + price + '$</span>';
+            if ( sales != '' ){
+              html += '<span class="sales" style="margin-left:30px;">' + sales + '</span>';
+            }
+            html += '<input type="hidden" value="'+ description +'">';
+            html += '</div></div></a></div>';
+
+            $('.best-sales .products-sales').append($(html));   
+          }
+        });
     });
 
 
@@ -110,24 +148,40 @@ $( function() {
       max: 100,
       values: [ 20, 50 ],
       slide: function( event, ui ) {
-        $( "#amount1" ).text( ui.values[ 0 ]*10 + "$");
+        $( "#amount1" ).text( ui.values[ 0 ]*2 + "$");
         $( "#amount1" ).css('left', ui.values[ 0 ]-7+"%");
-        $( "#amount2" ).text( ui.values[ 1 ]*10 + "$");
+        $( "#amount2" ).text( ui.values[ 1 ]*2 + "$");
         $( "#amount2" ).css('left', ui.values[ 1 ]-7+"%");
         var $grid = $('.grid').isotope({
           itemSelector: '.product-elements',
           filter: function() {
-            var number1 = ui.values[0]*10;
-            var number2 = ui.values[1]*10;
+            var number1 = ui.values[0]*2;
+            var number2 = ui.values[1]*2;
             var price = $(this).find('span').text();
             return parseInt( price, 10 ) > number1 && parseInt( price, 10 ) < number2;
           }
         });
       }
     });
-    $( "#amount1" ).text(  $( "#slider-range" ).slider( "values", 0 )*10 + "$");
+    $( "#amount1" ).text(  $( "#slider-range" ).slider( "values", 0 )*2 + "$");
     $( "#amount1" ).css('left', $( "#slider-range" ).slider( "values", 0 )-7+"%");
-    $( "#amount2" ).text(  $( "#slider-range" ).slider( "values", 1 )*10 + "$");
+    $( "#amount2" ).text(  $( "#slider-range" ).slider( "values", 1 )*2 + "$");
     $( "#amount2" ).css('left', $( "#slider-range" ).slider( "values", 1 )-7+"%");
 
+
+    /*
+    * Modal show product
+    */
+    $('body').on('click','a.tap', function() {
+          var img = $(this).find('img').attr('src');
+          var name = $(this).find('h3').text();
+          var price = $(this).find('span').first().text();
+          var description = $(this).find('input').val();
+
+          $('.imagepreview').attr('src', img);
+          $('.modal h4').text(name);
+          $('.modal p').text(price);
+          $('.modal h5').text(description);
+          $('#imagemodal').modal('show');  
+    });
 });
